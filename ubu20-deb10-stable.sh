7e6f935a56143c52aa1075792c47534dd93a77e5
madewgn <lab@madewgn.eu.org>
2024-06-23 22:47:22 +0800

    update

--- ubu20-deb10-stable.sh
+++ ubu20-deb10-stable.sh
@@ -408,7 +408,9 @@ function install_xray() {
     bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install -u www-data --version $latest_version
 
     # // Ambil Config Server
-    wget -O /etc/xray/config.json "${REPO}config/config.json" >/dev/null 2>&1
+    wget -O /etc/xray/trojan.json "${REPO}config/trojan.json" >/dev/null 2>&1
+    wget -O /etc/xray/vmess.json "${REPO}config/vmess.json" >/dev/null 2>&1  
+    wget -O /etc/xray/vless.json "${REPO}config/vless.json" >/dev/null 2>&1  
     wget -O /etc/systemd/system/runn.service "${REPO}files/runn.service" >/dev/null 2>&1
     #chmod +x /usr/local/bin/xray
     domain=$(cat /etc/xray/domain)
